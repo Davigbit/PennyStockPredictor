93e438eed381656f8643052cf87e43c81838f7d5
Davi Gava Bittencourt <davigavabittencourt@gmail.com>
2024-01-23 23:10:05 -0500

    Random Forest

--- Data.py
+++ Data.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 files = ['BTE.csv', 'Interests.csv', 'Natural Gas.csv', 'S&P 500.csv', 'WTI.csv']
 
@@ -28,9 +29,22 @@ dfs['df_WTI_csv'] = dfs['df_WTI_csv'].rename(columns={'value': 'WTI (USD)'})
 
 result_df = pd.concat([dfs['df_BTE_csv']['Date'], dfs['df_BTE_csv']['BTE (CAD)'], dfs['df_BTE_csv']['BTE Volume'], 
                        dfs['df_Interests_csv']['Interest Rate (%)'], dfs['df_Natural_Gas_csv']['Natural Gas (USD)'], 
-                       dfs['df_S&P_500_csv']['S&P 500 (USD)'], dfs['df_WTI_csv']['WTI (USD)']], axis=1)
+                       dfs['df_S&P_500_csv']['S&P 500 (USD)'], 
+                       dfs['df_WTI_csv']['WTI (USD)']], axis=1)
 
 result_df = result_df.dropna()
+
+def assign_change(column):
+    return column.diff().shift(-1).apply(lambda x: -1 if x > 0 else (1 if x < 0 else 0))
+
+result_df['BTE_change'] = assign_change(result_df['BTE (CAD)'])
+result_df['Volume_change'] = assign_change(result_df['BTE Volume'])
+result_df['Interest_change'] = assign_change(result_df['Interest Rate (%)'])
+result_df['Natural_Gas_change'] = assign_change(result_df['Natural Gas (USD)'])
+result_df['S&P_500_change'] = assign_change(result_df['S&P 500 (USD)'])
+result_df['WTI_change'] = assign_change(result_df['WTI (USD)'])
+
+result_df = result_df[result_df['BTE_change'] != 0]
 
 latest_date = result_df['Date'].max()
 
@@ -39,5 +53,3 @@ df_after_2023 = result_df.loc[result_df['Date'] > '2023-01-01']
 
 df_before_2023.set_index('Date', inplace=True)
 df_after_2023.set_index('Date', inplace=True)
-
-print(df_before_2023)
--- model.py
+++ model.py
@@ -1,47 +1,34 @@
-import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_absolute_error, mean_absolute_percentage_error
-from sklearn.linear_model import Ridge
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler
 import subprocess
 
 subprocess.run(['python', 'Data.py'])
 
 from Data import df_before_2023 as df
 
-plt.figure(figsize=(6,4))
-sns.heatmap(df.corr(), cmap='Blues', annot=True, fmt=".2f")
+#plt.figure(figsize=(6,4))
+#sns.heatmap(df.corr(), cmap='Blues', annot=True, fmt=".2f")
 
-train, test = train_test_split(df, test_size=0.4, shuffle=True)
+X = df.drop('BTE_change', axis=1)
+Y = df['BTE_change'].astype(int)
 
-X_train = train.loc[:, train.columns != 'BTE (CAD)']
-Y_train = train.loc[:, ['BTE (CAD)']]
-
-X_test = test.loc[:, test.columns != 'BTE (CAD)']
-Y_test = test.loc[:, ['BTE (CAD)']]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, shuffle=True, random_state=42)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-alphas = np.logspace(-1, 1, 500)
-param_grid = {'alpha': alphas}
+random_forest_model = RandomForestClassifier(class_weight='balanced', random_state=42)
+random_forest_model.fit(X_train_scaled, Y_train)
 
-grid_search = GridSearchCV(Ridge(), param_grid, cv=5)
-grid_search.fit(X_train_scaled, Y_train)
+Y_pred_rf = random_forest_model.predict(X_test_scaled)
+binary_predictions_rf = np.round(Y_pred_rf).astype(int)
 
-best_alpha = grid_search.best_params_['alpha']
-print(f'Best Alpha: {best_alpha}')
-
-ridge_model = Ridge(alpha=best_alpha)
-ridge_model.fit(X_train_scaled, Y_train)
-
-Y_pred = ridge_model.predict(X_test_scaled)
-
-print(f'Mean Absolute Percentage Error: {mean_absolute_percentage_error(Y_test, Y_pred):.3f}%')
-print(f'Mean Absolute Error: {mean_absolute_error(Y_test, Y_pred):.3f}')
-print(f'R2 Score: {r2_score(Y_test, Y_pred):.3f}')
+accuracy_rf = accuracy_score(Y_test, binary_predictions_rf)
+print(f'Accuracy on Validation Set: {accuracy_rf}')
+print(classification_report(Y_test, binary_predictions_rf))
